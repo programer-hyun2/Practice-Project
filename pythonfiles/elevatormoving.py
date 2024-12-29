a6dd9badd715811385cced8604ea8a2700ce15ff
Lee Seunghyun <73006036+programer-hyun2@users.noreply.github.com>
2024-12-29 20:25:20 +0900

    Delete pythonfiles directory

--- pythonfiles/elevatormoving.py
+++
@@ -1,9 +0,0 @@
-def solution(fl):
-    elevatormoves = 0
-    currentfloor = fl[0]
-    for floor in fl:
-        elevatormoves += abs(floor - currentfloor)
-        currentfloor = floor
-    return elevatormoves
-
-print(solution([1,2,5,4,2]))
--- pythonfiles/pythoncv2/test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-file = 'image.jpg'
-img = cv2.imread(file)
-img = cv2.resize(img,(300,300))
-gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("image",img)
-cv2.imshow("image-gray",gimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
